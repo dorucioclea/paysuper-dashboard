a9250076ecae3a9adcfc02c91bbbc7fbe653d41b
Temarie <temarie@ya.ru>
2019-12-12 15:08:01 +0500

    fix description field, fetch a list after create payout

--- src/pages/ProjectKeyProductPage.vue
+++ src/pages/ProjectKeyProductPage.vue
@@ -24,7 +24,7 @@ export default {
 
   data() {
     return {
-      langFields: ['cover.images', 'name'],
+      langFields: ['cover.images', 'name', 'description'],
       keyProductLocal: null,
       isSkuUnique: true,
       isDisablePlatformConfirmOpened: false,
